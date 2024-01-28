33244bb513bf2c8f868f9bd325c37a6caaeb265b
jubalclary <jubalclary@gmail.com>
2024-01-28 15:05:03 -0500

    Remove all the default react stuff

---
+++ src/Components/MainText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainText (props) {
+    return (
+        <div>
+            <h1>Welcome to the tournament maker!</h1>
+        </div>
+    )
+}
+
+export default MainText
